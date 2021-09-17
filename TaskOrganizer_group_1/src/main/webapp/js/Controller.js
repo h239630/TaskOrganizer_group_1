2307ec31e1301b5fef8a7b45680b6ecb7a562479
Mikael Lindebrekke <lindebrekkem@gmail.com>
2021-09-17 16:10:12 +0200

    Foreløpig forslag

---
+++ TaskOrganizer_group_1/src/main/webapp/js/Controller.js
@@ -0,0 +1,71 @@
+"use strict"
+
+enableaddtask()
+{
+	const tasklist = document.querySelector("task-list");
+	tasklist.enableaddtask();
+}
+
+addtaskCallback()
+{
+	const tasklist = document.querySelector("task-list");
+	tasklist.enableaddtask();
+	tasklist.addtaskCallback(
+		() => { 
+			console.log("Click event on 'New task button'")
+		});
+}
+
+changestatusCallback()
+{
+	const tasklist = document.querySelector("task-list");
+	tasklist.changestatusCallback(
+		(id, newStatus) => {
+			console.log(`User chose ${newStatus} for task ${id}`)
+		});
+}
+
+deletetaskCallback()
+{
+	const tasklist = document.querySelector("task-list");
+	tasklist.deletetaskCallback(
+		(id) => {
+			console.log(`Click event on delete button of task ${id}`)
+		});
+}
+
+
+noTask() 
+{
+	const tasklist = document.querySelector("task-list");
+	tasklist.noTask();	
+}
+
+showTask() 
+{
+	const tasklist = document.querySelector("task-list");
+	const newtask = {
+	"id": 5,
+	"title": "Do DAT152 home work",
+	"status": "ACTIVE"
+	};
+	tasklist.showTask(newtask);
+}
+
+updateTask() 
+{
+	const tasklist = document.querySelector("task-list");
+	const status = {
+	"id": 1,
+	"status": "ACTIVE"
+	};
+	tasklist.updateTask(status);
+}
+
+removeTask() 
+{
+	const tasklist = document.querySelector("task-list");
+	tasklist.removeTask(1);	
+}
+
+
